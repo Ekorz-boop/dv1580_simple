50aaaedd69832e04228614b1e089ccb8851f8984
Simon <muckot@gmail.com>
2024-09-18 16:48:19 +0200

    changes

--- add.c
+++ add.c
@@ -8,8 +8,7 @@ int main(int argc, char *argv[]){
   int a = atoi(argv[1]);
   int b = atoi(argv[2]);
   if (a < 0 || b < 0) {
-    printf("-ERROR-\n");
-    return 1;
+    return 0;
   }
   printf("%d\n", a+b);
   return 0;
