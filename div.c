cab6c2d3c5af6dd7a77c785093b9570957e6faa3
Simon <muckot@gmail.com>
2024-09-18 16:33:59 +0200

    add stuff

--- div.c
+++ div.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+// Implement a solution that takes the first two arguments (argv[1] (a1), argv[2] (a2)) and calculate a1/a2. 
+// Here, verify that the arguments are real numbers; see [2] for examples of how to do this. 
+// If a2 equals 0, do not do the calculation but print "DIV ZERO NOT ALLOWED!\n". 
 
+int main(int argc, char *argv[]){
+  // Verify that arguments are real numbers
+  int x1 = argv[0].isdigit();
+  int x2 = argv[1].isdigit();
+  if (x1 != 0 && x2 != 0) {
+    double a1 = atof(argv[0]);
+    double a2 = atof(argv[1]);
+    if (a2 == 0) {
+      printf("DIV ZERO NOT ALLOWED!\n");
+    } else {
+      printf("%f\n", a1/a2);
+    }
+  }
   return 0;
 }
--- mult.c
+++ mult.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 // Implement a solution that takes the first two arguments (argv[1] (a1), argv[2] (a2)) and calculate a1*a2. Here, verify that the arguments are real numbers.
 
 int main(int argc, char *argv[]){
   // Verify that arguments are real numbers
-  
-
-  double a1 = atof(argv[1]);
-  double a2 = atof(argv[2]);
-  printf("%f\n", a1*a2);
+  int x1 = argv[0].isdigit();
+  int x2 = argv[1].isdigit();
+  if (x1 != 0 && x2 != 0) {
+    double a1 = atof(argv[0]);
+    double a2 = atof(argv[1]);
+    printf("%f\n", a1*a2);
+  }
   return 0;
 }
