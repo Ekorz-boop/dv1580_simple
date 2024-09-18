eb9b247790d382e79acaa61403209e866f3f2125
Simon <muckot@gmail.com>
2024-09-18 16:40:35 +0200

    add astuff

--- add.c
+++ add.c
@@ -5,11 +5,11 @@
 // Implement a solution that takes the first two arguments (argv[1], argv[2]) and adds them together. At this point, assume the arguments are positive INTEGERS only. 
 
 int main(int argc, char *argv[]){
-  int x1 = isdigit(argv[1]);
-  int x2 = isdigit(argv[2]);
+  int x1 = isdigit(argv[0]);
+  int x2 = isdigit(argv[1]);
   if (x1 != 0 && x2 != 0) {
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
+    int a = atoi(argv[0]);
+    int b = atoi(argv[1]);
     printf("%d\n", a+b);
   }
   else {
--- sub.c
+++ sub.c
@@ -5,11 +5,11 @@
 // Implement a solution that takes the first two arguments (argv[1] (a1), argv[2] (a2)) and calculate a1-a2. At this point, assume the arguments are positive INTEGERS only, but note. The result can be negative. 
 
 int main(int argc, char *argv[]){
-  int x1 = isdigit(argv[1]);
-  int x2 = isdigit(argv[2]);
+  int x1 = isdigit(argv[0]);
+  int x2 = isdigit(argv[1]);
   if (x1 != 0 && x2 != 0) {
-    int a1 = atoi(argv[1]);
-    int a2 = atoi(argv[2]);
+    int a1 = atoi(argv[0]);
+    int a2 = atoi(argv[1]);
     printf("%d\n", a1-a2);
   }
   else {
